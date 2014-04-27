65cbb50ad1bc4ec45ffc9efba7c53920872d2120
mhadzic2 <merimah.mh@gmail.com>
2014-04-27 19:24:00 +0200

    forme - eclipse

---
+++ Interfejsi/src/org/eclipse/wb/swing/ZatvaranjeZahtjeva.java
@@ -0,0 +1,202 @@
+package org.eclipse.wb.swing;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+
+public class ZatvaranjeZahtjeva {
+
+	private JFrame frmZatvaranjeZahtjeva;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ZatvaranjeZahtjeva window = new ZatvaranjeZahtjeva();
+					window.frmZatvaranjeZahtjeva.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ZatvaranjeZahtjeva() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmZatvaranjeZahtjeva = new JFrame();
+		frmZatvaranjeZahtjeva.setTitle("Zatvaranje zahtjeva");
+		frmZatvaranjeZahtjeva.setBounds(100, 100, 336, 373);
+		frmZatvaranjeZahtjeva.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		
+		JButton btnZatvoriZahtjev = new JButton("Zatvori zahtjev");
+		GroupLayout groupLayout = new GroupLayout(frmZatvaranjeZahtjeva.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 297, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(13, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(221, Short.MAX_VALUE)
+					.addComponent(btnZatvoriZahtjev)
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 266, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnZatvoriZahtjev)
+					.addContainerGap(37, Short.MAX_VALUE))
+		);
+		
+		JLabel lblImeIPrezime = new JLabel("Ime i prezime klijenta:");
+		lblImeIPrezime.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		JLabel lblTipUreaja = new JLabel("Tip ure\u0111aja:");
+		lblTipUreaja.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		JLabel lblIdZahtjeva = new JLabel("ID zahtjeva:");
+		lblIdZahtjeva.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		JLabel lblKomentar = new JLabel("Komentar:");
+		lblKomentar.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setEditable(false);
+		textField_2.setColumns(10);
+		
+		JTextArea textArea = new JTextArea();
+		
+		JLabel lblGarancija = new JLabel("Garancija:");
+		lblGarancija.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		
+		JLabel lblCijena = new JLabel("Cijena:");
+		lblCijena.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblKomentar, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)
+						.addComponent(lblImeIPrezime, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)
+						.addComponent(lblIdZahtjeva, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)
+						.addComponent(lblTipUreaja, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)
+						.addComponent(lblGarancija, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)
+						.addComponent(lblCijena, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
+							.addComponent(textField_3)
+							.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(textField_2)
+							.addComponent(textField_1)
+							.addComponent(textArea))
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
+					.addGap(43))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap(18, Short.MAX_VALUE)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblIdZahtjeva)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblImeIPrezime))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblTipUreaja))
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+								.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblCijena))
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(13)
+							.addComponent(lblGarancija)
+							.addPreferredGap(ComponentPlacement.RELATED, 126, Short.MAX_VALUE)
+							.addComponent(lblKomentar)))
+					.addContainerGap())
+		);
+		
+		JRadioButton rdbtnDa = new JRadioButton("Da");
+		
+		JRadioButton rdbtnNe = new JRadioButton("Ne");
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(17)
+					.addComponent(rdbtnDa)
+					.addGap(5)
+					.addComponent(rdbtnNe)
+					.addContainerGap(22, Short.MAX_VALUE))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addComponent(rdbtnDa)
+						.addComponent(rdbtnNe))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		panel_1.setLayout(gl_panel_1);
+		panel.setLayout(gl_panel);
+		frmZatvaranjeZahtjeva.getContentPane().setLayout(groupLayout);
+	}
+}
