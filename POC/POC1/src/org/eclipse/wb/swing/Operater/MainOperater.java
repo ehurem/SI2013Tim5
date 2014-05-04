787d792a15affa2f8862311e779129a9c2e5ccb0
mhadzic2 <merimah.mh@gmail.com>
2014-05-04 23:17:31 +0200

    Haman gotove forme

--- POC/POC1/src/org/eclipse/wb/swing/Operater/MainOperater.java
+++ POC/POC1/src/org/eclipse/wb/swing/Operater/MainOperater.java
@@ -7,7 +7,10 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
+import javax.swing.ButtonGroup;
+import javax.swing.ComboBoxEditor;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
@@ -18,12 +21,25 @@ import javax.swing.JTabbedPane;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 
+import Models.*;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.sql.Date;
+import java.util.Enumeration;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
 public class MainOperater {
 
 	private JFrame frmInterfejsZaOperatera;
 	private JTextField textField_1;
 	private JTextField textField_2;
-
+	private static ArrayList<Klijent>_klijenti;
+	private static ArrayList<Zahtjev>_zahtjevi;
+	//the client we make if there is no one in the database already
+	private static Klijent _novi;
 	/**
 	 * Launch the application.
 	 */
@@ -31,6 +47,19 @@ public class MainOperater {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					set_klijenti(new ArrayList<Klijent>());
+					set_zahtjevi(new ArrayList<Zahtjev>());
+					set_novi(new Klijent());
+
+					//an example of a client that we input into list
+					Klijent primjer = new Klijent();
+					primjer.set_imeIPrezime("Mujo Mujic");
+					primjer.set_adresa("Skenderija");
+					primjer.setBrojTelefona("012345678");
+					primjer.setEmail("mujom@gmail.com");
+					
+					get_klijenti().add(primjer);
+					
 					MainOperater window = new MainOperater();
 					window.frmInterfejsZaOperatera.setVisible(true);
 				} catch (Exception e) {
@@ -48,13 +77,22 @@ public class MainOperater {
 	}
 
 	/**
+	 * Message Box form.
+	 */
+	public static void infoBox(String infoMessage, String naslov)
+    {
+        JOptionPane.showMessageDialog(null, infoMessage, "" + naslov, JOptionPane.INFORMATION_MESSAGE);
+    }
+	
+	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
 		frmInterfejsZaOperatera = new JFrame();
 		frmInterfejsZaOperatera.setTitle("Interfejs za operatera");
 		frmInterfejsZaOperatera.setBounds(100, 100, 377, 433);
-		frmInterfejsZaOperatera.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frmInterfejsZaOperatera.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		GroupLayout groupLayout = new GroupLayout(frmInterfejsZaOperatera.getContentPane());
@@ -70,12 +108,22 @@ public class MainOperater {
 		JPanel panel = new JPanel();
 		tabbedPane.addTab("Unos zahtjeva", null, panel, null);
 		
-		JButton button = new JButton("Unesi");
+		final JComboBox comboBox_1 = new JComboBox();
+		for (Klijent k : _klijenti) {
+			comboBox_1.addItem(k);
+		}
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		
 		JButton btnNoviKlijent = new JButton("Novi klijent");
+		
+		btnNoviKlijent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DodajKlijenta zaDodavanje = new DodajKlijenta();
+				zaDodavanje.main(null, get_klijenti(), get_novi());
+			}
+		});
 		
 		JLabel label = new JLabel("ID zahtjeva:");
 		label.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -95,7 +143,7 @@ public class MainOperater {
 		JLabel label_4 = new JLabel("Garancija:");
 		label_4.setHorizontalAlignment(SwingConstants.RIGHT);
 		
-		JTextArea textArea = new JTextArea();
+		final JTextArea textArea = new JTextArea();
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
@@ -103,9 +151,66 @@ public class MainOperater {
 		JPanel panel_3 = new JPanel();
 		panel_3.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		
-		JRadioButton radioButton = new JRadioButton("Da");
+		final JRadioButton radioButton = new JRadioButton("Da");
 		
 		JRadioButton radioButton_1 = new JRadioButton("Ne");
+		
+		//we need to group JRadioButtons in order to get only one of them selected at the time
+		final ButtonGroup grupaGarancija = new ButtonGroup();
+		grupaGarancija.add(radioButton);
+		grupaGarancija.add(radioButton_1);
+		
+		JButton button = new JButton("Unesi");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try{
+					Zahtjev noviZahtjev = new Zahtjev();
+					noviZahtjev.setID(Long.parseLong(textField_1.getText()));
+					
+					//try{String imeIzabranog = Object.toString(comboBox_1.getSelectedItem());
+					/*for(Klijent k : _klijenti){
+						if(k.get_imeIPrezime() == imeIzabranog){
+							noviZahtjev.setKlijent(k);
+							break;
+						}
+					}}
+					catch(Exception ex) {infoBox(ex.getMessage(), "greska kod konverzije");}
+					*/
+					
+					noviZahtjev.setKlijent((Klijent)comboBox_1.getSelectedItem());
+					noviZahtjev.setTipUredaja(textField_2.getText());
+
+					Enumeration elementiGrupe = grupaGarancija.getElements();
+					while(elementiGrupe.hasMoreElements()){
+						JRadioButton dugme = (JRadioButton)elementiGrupe.nextElement();
+						if(dugme.isSelected()){
+							if(dugme == radioButton) noviZahtjev.setGarancija(true);
+							else noviZahtjev.setGarancija(false);
+							break;
+						}
+					}
+					
+					noviZahtjev.setKomentar(textArea.getText());
+					
+					java.util.Date today = new java.util.Date();
+					java.sql.Date datumO = new java.sql.Date(today.getTime());
+					
+					noviZahtjev.setDatumOtvaranja(datumO);
+					
+					//infoBox(Integer.toString(noviZahtjev.getDatumOtvaranja().getYear()), "Datum otvaranja");
+					
+					noviZahtjev.setStatus("otvoren");
+					
+					get_zahtjevi().add(noviZahtjev);
+					
+				}
+				catch(Exception izuzetak)
+				{
+					infoBox(izuzetak.getMessage(), "Greska u unosu !");
+				}
+			}
+		});
+		
 		GroupLayout gl_panel_3 = new GroupLayout(panel_3);
 		gl_panel_3.setHorizontalGroup(
 			gl_panel_3.createParallelGroup(Alignment.LEADING)
@@ -126,7 +231,6 @@ public class MainOperater {
 		);
 		panel_3.setLayout(gl_panel_3);
 		
-		JComboBox comboBox_1 = new JComboBox();
 		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
 		gl_panel_2.setHorizontalGroup(
 			gl_panel_2.createParallelGroup(Alignment.TRAILING)
@@ -274,5 +378,29 @@ public class MainOperater {
 		panel_4.setLayout(gl_panel_4);
 		panel_1.setLayout(gl_panel_1);
 		frmInterfejsZaOperatera.getContentPane().setLayout(groupLayout);
+	}
+
+	private static ArrayList<Klijent> get_klijenti() {
+		return _klijenti;
+	}
+
+	private static void set_klijenti(ArrayList<Klijent> _klijenti) {
+		MainOperater._klijenti = _klijenti;
+	}
+
+	private static ArrayList<Zahtjev> get_zahtjevi() {
+		return _zahtjevi;
+	}
+
+	private static void set_zahtjevi(ArrayList<Zahtjev> _zahtjevi) {
+		MainOperater._zahtjevi = _zahtjevi;
+	}
+
+	public static Klijent get_novi() {
+		return _novi;
+	}
+
+	public static void set_novi(Klijent _novi) {
+		MainOperater._novi = _novi;
 	}
 }
