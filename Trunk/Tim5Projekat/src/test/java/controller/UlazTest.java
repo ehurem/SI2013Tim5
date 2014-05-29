afadf867e047f0c053de6c756a5c9f393a2191e3
AlenIsmic <alen.ismic@gmail.com>
2014-05-29 12:11:13 +0200

    generisane metode za testove

---
+++ Trunk/Tim5Projekat/src/test/java/controller/UlazTest.java
@@ -0,0 +1,27 @@
+package controller;
+
+import junit.framework.TestCase;
+
+public class UlazTest extends TestCase {
+
+	public void testProvjeraUlaznihPodataka() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testEncryptPassword() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testByteToHex() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGet_zaposlenik() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSet_zaposlenik() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ Trunk/Tim5Projekat/src/test/java/org/eclipse/wb/swing/Administrator/MainTest.java
@@ -0,0 +1,28 @@
+package org.eclipse.wb.swing.Administrator;
+
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+
+	public void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testMain1() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testInfoBox() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	
+	public void testGet_zaposlenik() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSet_zaposlenik() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
