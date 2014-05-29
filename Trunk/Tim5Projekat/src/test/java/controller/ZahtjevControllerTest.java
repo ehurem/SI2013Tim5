49aef1545d4b90bb482ab2c5a7c4cc132978923a
Esad Hasanbegovic <ehasanbego2@etf.unsa.ba>
2014-05-30 01:28:22 +0200

    Dodana test klasa

---
+++ Trunk/Tim5Projekat/src/test/java/controller/ZahtjevControllerTest.java
@@ -0,0 +1,29 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ZahtjevControllerTest {
+
+	@Test
+	public void testZahtjevController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPopuniPodatke() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpisivanjeZatvorenogZahtjeva() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPopunjavanjeListePreuzetihZahtjeva() {
+		fail("Not yet implemented");
+	}
+
+}
