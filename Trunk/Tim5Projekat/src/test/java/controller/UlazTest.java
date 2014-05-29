975e239fa6dc0e801790e0c1e0e8375de1e2b9fe
AlenIsmic <alen.ismic@gmail.com>
2014-05-29 17:46:10 +0200

    testovi za login

--- Trunk/Tim5Projekat/src/test/java/controller/UlazTest.java
+++ Trunk/Tim5Projekat/src/test/java/controller/UlazTest.java
@@ -1,27 +1,51 @@
 package controller;
 
+import javax.swing.JTextField;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
+@RunWith(JUnit4.class)
 public class UlazTest extends TestCase {
 
-	public void testProvjeraUlaznihPodataka() {
-		fail("Not yet implemented"); // TODO
+	@Test(expected = java.lang.Exception.class)
+	public void testProvjeraUlaznihPodatakaBezUnosa() throws Exception {
+		JTextField t_korisnickoIme = new JTextField("");
+		JTextField t_sifra = new JTextField("");
+		Ulaz.provjeraUlaznihPodataka(t_korisnickoIme, t_sifra);
+	}
+	@Test(expected = java.lang.Exception.class)
+	public void testProvjeraUlaznihPodatakaNepostojeciKorisnik() throws Exception {
+		JTextField t_korisnickoIme = new JTextField("Korisnik");
+		JTextField t_sifra = new JTextField("lijevaSifra");
+		Ulaz.provjeraUlaznihPodataka(t_korisnickoIme, t_sifra);
+	}	
+	@Test
+	public void testProvjeraUlaznihPodatakaUspjesnaAdministrator() throws Exception {
+		JTextField t_korisnickoIme = new JTextField("admin");
+		JTextField t_sifra = new JTextField("sifra");
+		Assert.assertEquals(Ulaz.provjeraUlaznihPodataka(t_korisnickoIme, t_sifra), "Administrator");
+	}
+	@Test
+	public void testProvjeraUlaznihPodatakaUspjesnaOperater() throws Exception {
+		JTextField t_korisnickoIme = new JTextField("operater");
+		JTextField t_sifra = new JTextField("sifra");
+		Assert.assertEquals(Ulaz.provjeraUlaznihPodataka(t_korisnickoIme, t_sifra), "Operater");
+	}
+	@Test
+	public void testProvjeraUlaznihPodatakaUspjesnaServiser() throws Exception {
+		JTextField t_korisnickoIme = new JTextField("serviser");
+		JTextField t_sifra = new JTextField("sifra");
+		Assert.assertEquals(Ulaz.provjeraUlaznihPodataka(t_korisnickoIme, t_sifra), "Serviser");
 	}
 
 	public void testEncryptPassword() {
-		fail("Not yet implemented"); // TODO
+		String password = "sifra";
+		String sha1PASSWORD = "4259031dc85f451a2b7731e8f5ea93193dad63ad";
+		Assert.assertEquals(password, sha1PASSWORD);
 	}
-
-	public void testByteToHex() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	public void testGet_zaposlenik() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	public void testSet_zaposlenik() {
-		fail("Not yet implemented"); // TODO
-	}
-
 }
