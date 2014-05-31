0f109d724cdfac9a4c0bb44527eb8331fcc82fec
AlenIsmic <alen.ismic@gmail.com>
2014-05-31 11:24:37 +0200

    Testovi za DodavanjeZaposlenika

--- Trunk/Tim5Projekat/src/test/java/controller/DodavanjeZaposlenikaTest.java
+++ Trunk/Tim5Projekat/src/test/java/controller/DodavanjeZaposlenikaTest.java
@@ -20,6 +20,24 @@ public class DodavanjeZaposlenikaTest {
 	private static Long id;
 	
 	@Test
+	public void testDodajZaposlenikaUnosLos() throws Exception {
+		JTextField t_imeIPrezime = new JTextField("" );
+		JTextField t_mjestoStanovanja= new JTextField("Mjesto");
+		JTextField t_brojTelefona= new JTextField("12312311232");
+		JTextField t_emailAdresa= new JTextField("nekimanil.com");
+		JTextField t_korisnickaSifra= new JTextField("sifra");
+		JTextField t_korisnickoIme= new JTextField("alen");
+		String[] niz = new String[1];
+		niz[0] = "Administrator";
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		JComboBox c_privilegije = new JComboBox(niz);
+		JTextField t_datumRodjenja = new JTextField("1991-06-01");
+		id = DodavanjeZaposlenika.DodajZaposlenik(t_imeIPrezime, t_mjestoStanovanja, t_brojTelefona, t_emailAdresa, t_korisnickaSifra, t_korisnickoIme, c_privilegije, t_datumRodjenja);
+		Boolean test =  id != 0;
+		Assert.assertTrue(test);
+		
+	}
+	@Test
 	public void testDodajZaposlenikDobriPodaci() throws Exception {
 		JTextField t_imeIPrezime = new JTextField("Alen Ismic" );
 		JTextField t_mjestoStanovanja= new JTextField("Mjesto");
