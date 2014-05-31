1563f397d89ade8458a85390d96ea2641872c64f
aimamovic3 <aimamovic3@etf.unsa.ba>
2014-05-31 22:12:47 +0200

    ispravka testa

--- Trunk/Tim5Projekat/src/test/java/controller/serviserKontrolerTest.java
+++ Trunk/Tim5Projekat/src/test/java/controller/serviserKontrolerTest.java
@@ -6,15 +6,44 @@ import java.util.Date;
 import java.util.List;
 
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
+import Models.Klijent;
 import Models.Zahtjev;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class serviserKontrolerTest {
+import tim5.si.unsa.ba.Tim5Projekat.HibernateUtil;
 
+public class serviserKontrolerTest {
+	private static Long _zaposlenik;
+	private static Long c1;
+	private static Integer c3;
+	private static JTextField textField_2;
+	private static Boolean dugme;
+	private static JTextArea textArea;
+	private static Long id;
+	private static Long idKlijenta;
 	
+
+	//setup
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		_zaposlenik = new Long(1);
+		c1 = new Long(1);
+		c3 =  new Integer(1);
+		textField_2 = new JTextField();
+		dugme = new Boolean(true);
+		textArea = new JTextArea();
+		id = new Long(0);
+		idKlijenta = new Long(0);
+}
 	@Test
 	public final void testUcitajOtvoreneZahtjeve() {
 		List <Zahtjev> zahtjevi = null;
@@ -40,8 +69,18 @@ public class serviserKontrolerTest {
 	}
 
 
-	@Test (expected = Exception.class)  //ako nema otvorenih zahtjeva u bazi baca exception
+   @Test
 	public final void testUzmiZahtjevNaIzvrsavanje() throws Exception {
+
+		Klijent k = new Klijent();
+		JTextField t = new JTextField("Niko Nikic");
+		JTextField t1 = new JTextField("033/123-456");
+		JTextField t2 = new JTextField("niko@nesto.com");
+		JTextField t3 = new JTextField("Negdje daleko");
+		idKlijenta = DodavanjeKlijenta.unesiKlijentaUBazu(k, t, t1, t2, t3);
+		textField_2.setText("veš mašina");
+		textArea.setText("Skače pri centrifugi");		
+		id = UnosZahtjeva.unesiZahtjevUBazu(_zaposlenik, idKlijenta, c3, textField_2, dugme, textArea);
 		List <Zahtjev> zahtjevi = serviserKontroler.ucitajOtvoreneZahtjeve();
 		int size = zahtjevi.size();
 		JTable tabela = new JTable();
@@ -51,6 +90,22 @@ public class serviserKontrolerTest {
 	    serviserKontroler.uzmiZahtjevNaIzvrsavanje(tabela, zahtjevi, 1, zahtjevi.size()-1);
 	    assertEquals(zahtjevi.size(),size-1);
 	//	}
+	}
+	@AfterClass
+	public static void tearDown()
+	{
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		
+		Zahtjev uneseni = (Zahtjev) session.get(Zahtjev.class, id); 
+		session.delete(uneseni); 
+		
+		Klijent testniKlijent = (Klijent) session.get(Klijent.class, idKlijenta);
+		session.delete(testniKlijent);
+		
+		tx.commit();
+		session.close();
 	}
 
 }
