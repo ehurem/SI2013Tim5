355d3b0440c38b01e8dfe940cf8154301e7409a7
khodzic2 <khodzic2@etf.unsa.ba>
2014-05-31 18:40:34 +0200

    Dodana još 4 unit testa za izvještaje

--- Trunk/Tim5Projekat/src/main/java/controller/IzvjestajiKontroler.java
+++ Trunk/Tim5Projekat/src/main/java/controller/IzvjestajiKontroler.java
@@ -110,7 +110,8 @@ public class IzvjestajiKontroler {
 				otvoreni++;	
 			}
 		}
-		}
+	}
 		return otvoreni;
+
 	}
 }
--- Trunk/Tim5Projekat/src/test/java/controller/IzvjestajiKontrolerTest.java
+++ Trunk/Tim5Projekat/src/test/java/controller/IzvjestajiKontrolerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import junit.framework.Assert;
 
@@ -53,24 +54,194 @@ public class IzvjestajiKontrolerTest {
 		
 	}
 	@Test
-    public void testirajRacunanjeZaradeTacno() throws Exception {
-		ArrayList<Zahtjev> zahtjevi = new ArrayList<Zahtjev>();
+    public void testirajRacunanjeZaradeTacno() {
+		List <Zahtjev> zahtjevi = new ArrayList<Zahtjev>();
 		Zahtjev prvi = new Zahtjev();
 		Zahtjev drugi = new Zahtjev();
-		prvi.set_cijena(200);
-		drugi.set_cijena(300);
 	    java.sql.Date jsqlD1 = java.sql.Date.valueOf( "2014-01-01" );
 	    java.sql.Date jsqlD2 = java.sql.Date.valueOf( "2014-01-01" );
 	    Calendar proba = Calendar.getInstance();
 	    proba.setTime(jsqlD1);
 		prvi.setDatumZatvaranja(jsqlD1);
 		drugi.setDatumZatvaranja(jsqlD1);
+		prvi.setDatumOtvaranja(jsqlD1);
+		drugi.setDatumOtvaranja(jsqlD1);
+		prvi.setGarancija(true);
+		drugi.setGarancija(true);
+		prvi.setID(1);
+		drugi.setID(1);
+		prvi.set_cijena(200);
+		drugi.set_cijena(300);
+		prvi.setKomentar("dada");
+        drugi.setKomentar("nene");
+        prvi.setStatus("otvoren");
+        drugi.setStatus("otvoren");
+        prvi.setKlijent(2);
+        drugi.setKlijent(2);
+        prvi.setPrioritet(1);
+        drugi.setPrioritet(1);
+        prvi.setTipUredaja("masina");
+        drugi.setTipUredaja("masina");
+        prvi.setZaposlenik((long) 300);
+        drugi.setZaposlenik((long) 3);
+        zahtjevi.clear();
+		zahtjevi.add(prvi);
+		zahtjevi.add(drugi);		
+		try {
+			Assert.assertEquals(IzvjestajiKontroler.sabiranjeCijenaZahtjevaZaOdabranuSedmicu(zahtjevi,proba.get(Calendar.WEEK_OF_YEAR)),500.0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+	@Test
+	public void dajZatvoreneTacno() {
+		List <Zahtjev> zahtjevi = new ArrayList<Zahtjev>();
+		Zahtjev prvi = new Zahtjev();
+		Zahtjev drugi = new Zahtjev();
+	    java.sql.Date jsqlD1 = java.sql.Date.valueOf( "2014-01-01" );
+	    java.sql.Date jsqlD2 = java.sql.Date.valueOf( "2014-01-01" );
+	    Calendar proba = Calendar.getInstance();
+	    proba.setTime(jsqlD1);
+		prvi.setDatumZatvaranja(jsqlD1);
+		drugi.setDatumZatvaranja(jsqlD1);
+		prvi.setDatumOtvaranja(jsqlD1);
+		drugi.setDatumOtvaranja(jsqlD1);
+		prvi.setGarancija(true);
+		drugi.setGarancija(true);
+		prvi.setID(1);
+		drugi.setID(1);
+		prvi.set_cijena(200);
+		drugi.set_cijena(300);
+		prvi.setKomentar("dada");
+        drugi.setKomentar("nene");
+        prvi.setStatus("otvoren");
+        drugi.setStatus("otvoren");
+        prvi.setKlijent(2);
+        drugi.setKlijent(2);
+        prvi.setPrioritet(1);
+        drugi.setPrioritet(1);
+        prvi.setTipUredaja("masina");
+        drugi.setTipUredaja("masina");
+        prvi.setZaposlenik((long) 300);
+        drugi.setZaposlenik((long) 3);
+        zahtjevi.clear();
 		zahtjevi.add(prvi);
 		zahtjevi.add(drugi);
-		
-		Assert.assertEquals(IzvjestajiKontroler.sabiranjeCijenaZahtjevaZaOdabranuSedmicu(zahtjevi,proba.get(Calendar.WEEK_OF_YEAR)),500);
-		
-		
+		Assert.assertEquals(IzvjestajiKontroler.dajBrojZatvorenih(zahtjevi, proba.get(Calendar.WEEK_OF_YEAR)),2);
 	}
+	@Test
+	public void dajZatvoreneNetacno() {
+		List <Zahtjev> zahtjevi = new ArrayList<Zahtjev>();
+		Zahtjev prvi = new Zahtjev();
+		Zahtjev drugi = new Zahtjev();
+	    java.sql.Date jsqlD1 = java.sql.Date.valueOf( "2014-01-01" );
+	    java.sql.Date jsqlD2 = java.sql.Date.valueOf( "2014-01-01" );
+	    Calendar proba = Calendar.getInstance();
+	    proba.setTime(jsqlD1);
+		prvi.setDatumZatvaranja(jsqlD1);
+		drugi.setDatumZatvaranja(jsqlD1);
+		prvi.setDatumOtvaranja(jsqlD1);
+		drugi.setDatumOtvaranja(jsqlD1);
+		prvi.setGarancija(true);
+		drugi.setGarancija(true);
+		prvi.setID(1);
+		drugi.setID(1);
+		prvi.set_cijena(200);
+		drugi.set_cijena(300);
+		prvi.setKomentar("dada");
+        drugi.setKomentar("nene");
+        prvi.setStatus("otvoren");
+        drugi.setStatus("otvoren");
+        prvi.setKlijent(2);
+        drugi.setKlijent(2);
+        prvi.setPrioritet(1);
+        drugi.setPrioritet(1);
+        prvi.setTipUredaja("masina");
+        drugi.setTipUredaja("masina");
+        prvi.setZaposlenik((long) 300);
+        drugi.setZaposlenik((long) 3);
+        zahtjevi.clear();
+		zahtjevi.add(prvi);
+		zahtjevi.add(drugi);
+		Assert.assertNotSame(IzvjestajiKontroler.dajBrojZatvorenih(zahtjevi, proba.get(Calendar.WEEK_OF_YEAR)),100);
+	}
+	
+	
+	@Test
+	public void dajOtvoreneNetacno() {
+		List <Zahtjev> zahtjevi = new ArrayList<Zahtjev>();
+		Zahtjev prvi = new Zahtjev();
+		Zahtjev drugi = new Zahtjev();
+	    java.sql.Date jsqlD1 = java.sql.Date.valueOf( "2014-01-01" );
+	    java.sql.Date jsqlD2 = java.sql.Date.valueOf( "2014-10-01" );
+	    Calendar proba = Calendar.getInstance();
+	    proba.setTime(jsqlD1);
+		prvi.setDatumZatvaranja(jsqlD2);
+		drugi.setDatumZatvaranja(jsqlD2);
+		prvi.setDatumOtvaranja(jsqlD1);
+		drugi.setDatumOtvaranja(jsqlD1);
+		prvi.setGarancija(true);
+		drugi.setGarancija(true);
+		prvi.setID(1);
+		drugi.setID(1);
+		prvi.set_cijena(200);
+		drugi.set_cijena(300);
+		prvi.setKomentar("dada");
+        drugi.setKomentar("nene");
+        prvi.setStatus("otvoren");
+        drugi.setStatus("otvoren");
+        prvi.setKlijent(2);
+        drugi.setKlijent(2);
+        prvi.setPrioritet(1);
+        drugi.setPrioritet(1);
+        prvi.setTipUredaja("masina");
+        drugi.setTipUredaja("masina");
+        prvi.setZaposlenik((long) 300);
+        drugi.setZaposlenik((long) 3);
+        zahtjevi.clear();
+		zahtjevi.add(prvi);
+		zahtjevi.add(drugi);
+		Assert.assertNotSame(IzvjestajiKontroler.dajBrojZatvorenih(zahtjevi, proba.get(Calendar.WEEK_OF_YEAR)),100);
+	}
+	
+	@Test
+	public void dajOtvoreneTacno() {
+		List <Zahtjev> zahtjevi = new ArrayList<Zahtjev>();
+		Zahtjev prvi = new Zahtjev();
+		Zahtjev drugi = new Zahtjev();
+	    java.sql.Date jsqlD1 = java.sql.Date.valueOf( "2014-01-01" );
+	    java.sql.Date jsqlD2 = java.sql.Date.valueOf( "2014-10-01" );
+	    Calendar proba = Calendar.getInstance();
+	    proba.setTime(jsqlD1);
+		prvi.setDatumZatvaranja(jsqlD2);
+		drugi.setDatumZatvaranja(jsqlD2);
+		prvi.setDatumOtvaranja(jsqlD1);
+		drugi.setDatumOtvaranja(jsqlD1);
+		prvi.setGarancija(true);
+		drugi.setGarancija(true);
+		prvi.setID(1);
+		drugi.setID(1);
+		prvi.set_cijena(200);
+		drugi.set_cijena(300);
+		prvi.setKomentar("dada");
+        drugi.setKomentar("nene");
+        prvi.setStatus("otvoren");
+        drugi.setStatus("otvoren");
+        prvi.setKlijent(2);
+        drugi.setKlijent(2);
+        prvi.setPrioritet(1);
+        drugi.setPrioritet(1);
+        prvi.setTipUredaja("masina");
+        drugi.setTipUredaja("masina");
+        prvi.setZaposlenik((long) 300);
+        drugi.setZaposlenik((long) 3);
+        zahtjevi.clear();
+		zahtjevi.add(prvi);
+		zahtjevi.add(drugi);
+		Assert.assertNotSame(IzvjestajiKontroler.dajBrojZatvorenih(zahtjevi, proba.get(Calendar.WEEK_OF_YEAR)),2);
+	}
+	
+	
 	
 }
