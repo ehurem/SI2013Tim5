81334e235cfcc9d42dfe1325746d5daf4737cec5
aimamovic3 <aimamovic3@etf.unsa.ba>
2014-05-31 02:45:19 +0200

    dodana metoda uzmiZahtjevNaIzvrsavanje

--- Trunk/Tim5Projekat/src/main/java/controller/TableHeaderMouseListener.java
+++ Trunk/Tim5Projekat/src/main/java/controller/TableHeaderMouseListener.java
@@ -13,13 +13,13 @@ import javax.swing.table.DefaultTableModel;
 import Models.Zahtjev;
 
 public class TableHeaderMouseListener extends MouseAdapter {
-  private serviserKontroler kontroler;  
+  private serviserKontroler metoda;  
   private JTable tabela;
   private List <Zahtjev> zahtjevi;
    public TableHeaderMouseListener(JTable tabela, List <Zahtjev> zahtjevi) {
        this.tabela = tabela;
        this.zahtjevi = zahtjevi;
-       kontroler = new serviserKontroler();
+       metoda = new serviserKontroler();
    }
     
    public void mouseClicked(MouseEvent event) {
@@ -29,13 +29,13 @@ public class TableHeaderMouseListener extends MouseAdapter {
        if (column==0) {   
     	   ((DefaultTableModel) tabela.getModel()).setRowCount(0);
            Collections.sort(zahtjevi);
-           kontroler.popuniTabelu(tabela, zahtjevi);
+           metoda.popuniTabelu(tabela, zahtjevi);
        }
    
        else {
     	   Collections.sort(zahtjevi, new Zahtjev.PoPrioritetu());
     	   ((DefaultTableModel) tabela.getModel()).setRowCount(0);
-    	   kontroler.popuniTabelu(tabela, zahtjevi);
+    	   metoda.popuniTabelu(tabela, zahtjevi);
        }
        	
    }
